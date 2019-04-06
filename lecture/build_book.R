58193849faf910e91be1f77e712126586e5463c8
Kohei <kohei@Koheis-MacBook-Pro-299.local>
2019-04-06 16:55:33 -0400

    Dynamic Game

--- lecture/build_book.R
+++ lecture/build_book.R
@@ -1,12 +1,13 @@
 # restart the session
 .rs.restartR()
+file.create("../docs/.nojekyll")
 
 # set the working directory
 setwd("lecture")
 
 # serve book
 bookdown::serve_book(dir = ".")
-file.create("../docs/.nojekyll")
+
 
 # render html book
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
@@ -14,5 +15,3 @@ bookdown::render_book("index.Rmd", "bookdown::gitbook")
 # render pdf book
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
-# publish book on RStudio Connect
-bookdown::publish_book()
