008f539d498013b4e0fa9738da75493cdfd50b6c
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2022-09-02 14:29:00 +0800

    checked A1

--- R/A1_functions.R
+++ R/A1_functions.R
@@ -3,10 +3,14 @@
 #-------------#
 # log likelihood function
 loglikelihood_A1 <- 
-  function(b, df) {
-    l <- df %>%
+  function(
+    b, 
+    df
+    ) {
+    l <- 
+      df %>%
       dplyr::group_by(i) %>%
-      dplyr::mutate(p = exp(b*x)/sum(exp(b*x))) %>%
+      dplyr::mutate(p = exp(b * x) / sum(exp(b * x))) %>%
       dplyr::ungroup() %>%
       dplyr::filter(y == 1)
     l <- mean(log(l$p))
