9fb30a57b252fa4603d0f5ca367205ef51a7aa1e
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2022-09-02 19:27:47 +0800

    checked assignment 1 and 3, need check on 2

--- R/a2_functions.R
+++ R/a2_functions.R
@@ -17,65 +17,111 @@ log_production <-
   }
 # static labor choice without optimization error
 log_labor_choice <- 
-  function(k, wage, omega, beta_0, beta_l, beta_k, sigma_eta) {
-    L <- 
-      ((1/wage) * 
-         exp(beta_0 + omega + sigma_eta^2/2) * 
-         beta_l * 
-         exp(k)^beta_k)^(1/(1 - beta_l))
-    l <- log(L)
+  function(
+    k, 
+    wage, 
+    omega, 
+    beta_0, 
+    beta_l, 
+    beta_k, 
+    sigma_eta
+    ) {
+    l <- 
+      (
+        (1/wage) * 
+          exp(beta_0 + omega + sigma_eta^2 / 2) * 
+          beta_l * 
+          exp(k)^beta_k
+        )^(1/(1 - beta_l))
+    l <- log(l)
     return(l)
   }
 
 # static labor choice with optimization error
 log_labor_choice_error <- 
-  function(k, wage, omega, beta_0, beta_l, beta_k, iota, sigma_eta) {
-    L <- 
-      ((1/wage) * 
+  function(
+    k, 
+    wage, 
+    omega, 
+    beta_0, 
+    beta_l, 
+    beta_k, 
+    iota, 
+    sigma_eta
+    ) {
+    l <- 
+      (
+        (1/wage) * 
          exp(beta_0 + omega + iota + sigma_eta^2/2) * 
          beta_l * 
-         exp(k)^beta_k)^(1/(1 - beta_l))
-    l <- log(L)
+         exp(k)^beta_k
+        )^(1/(1 - beta_l))
+    l <- log(l)
     return(l)
   }
 # investment choice
 investment_choice <-
-  function(k, omega, gamma, delta) {
-    I <- (delta + gamma * omega) * exp(k)
-    return(I)
+  function(
+    k, 
+    omega, 
+    gamma, 
+    delta
+    ) {
+    inv <- (delta + gamma * omega) * exp(k)
+    return(inv)
   }
 
 # compute moment for Olley-Pakes second stage
-moment_OP_2nd <- 
-  function(alpha, beta_0, beta_k, df_T, df_T_1st) {
-    moment <- df_T %>%
+moment_olleypakes_2nd <- 
+  function(
+    alpha, 
+    beta_0, 
+    beta_k, 
+    df_all, 
+    df_all_1st
+    ) {
+    moment <- 
+      df_all %>%
       dplyr::group_by(j) %>%
       dplyr::mutate(
         k_t_1 = dplyr::lag(k, 1),
-        I_t_1 = dplyr::lag(I, 1)
+        inv_t_1 = dplyr::lag(inv, 1)
       ) %>%
       dplyr::ungroup() %>%
-      dplyr::left_join(df_T_1st, by = c("j", "t")) %>%
+      dplyr::left_join(df_all_1st, by = c("j", "t")) %>%
       dplyr::filter(!is.na(phi_t_1)) %>%
       dplyr::mutate(
-        g_jt = y_error_tilde - beta_0 - beta_k * k - 
+        g_jt = 
+          y_error_tilde - beta_0 - beta_k * k - 
           alpha * (phi_t_1 - beta_0 - beta_k * k_t_1)
       )
     moment <-
-      cbind(moment$g_jt * moment$k,
-            moment$g_jt * moment$k_t_1,
-            moment$g_jt * moment$I_t_1)
-    moment <- apply(moment, 2, mean)
+      cbind(
+        moment$g_jt * moment$k,
+        moment$g_jt * moment$k_t_1,
+        moment$g_jt * moment$inv_t_1
+        )
+    moment <- 
+      apply(
+        moment, 
+        2, 
+        mean
+        )
     return(moment)
   }
 # compute objective function for Olley-Pakes second stage
-objective_OP_2nd <-
-  function(theta, df_T, df_T_1st, W) {
+objective_olleypakes_2nd <-
+  function(
+    theta, 
+    df_all, 
+    df_all_1st,
+    W
+    ) {
     alpha <- theta[1]
     beta_0 <- theta[2]
     beta_k <- theta[3]
     moment <-
-      moment_OP_2nd(alpha, beta_0, beta_k, df_T, df_T_1st)
+      moment_olleypakes_2nd(alpha, beta_0, beta_k, df_all, df_all_1st)
     objective <- t(moment) %*% W %*% moment
     return(objective)
   }
--- R/a3_functions.R
+++ R/a3_functions.R
@@ -1,7 +1,12 @@
 # compute indirect utility
 compute_indirect_utility <- 
-  function(df, beta, sigma, 
-           mu, omega) {
+  function(
+    df, 
+    beta, 
+    sigma, 
+    mu, 
+    omega
+    ) {
     # extract matrices
     X <- as.matrix(dplyr::select(df, dplyr::starts_with("x_")))
     p <- as.matrix(dplyr::select(df, p)) 
@@ -18,29 +23,70 @@ compute_indirect_utility <-
   }
 # compute choice
 compute_choice <-
-  function(X, M, V, e, beta, sigma, 
-           mu, omega) {
+  function(
+    X, 
+    M, 
+    V, 
+    e, 
+    beta, 
+    sigma,
+    mu, 
+    omega
+    ) {
     # constants
     T <- max(M$t)
     N <- max(V$i)
     J <- max(X$j)
     # make choice data
-    df <- expand.grid(t = 1:T, i = 1:N, j = 0:J) %>%
+    df <- 
+      expand.grid(
+        t = 1:T, 
+        i = 1:N, 
+        j = 0:J
+        ) %>%
       tibble::as_tibble() %>%
-      dplyr::left_join(V, by = c("i", "t")) %>%
-      dplyr::left_join(X, by = c("j")) %>%
-      dplyr::left_join(M, by = c("j", "t")) %>%
+      dplyr::left_join(
+        V, 
+        by = c("i", "t")
+        ) %>%
+      dplyr::left_join(
+        X, 
+        by = c("j")
+        ) %>%
+      dplyr::left_join(
+        M, 
+        by = c("j", "t")
+        ) %>%
       dplyr::filter(!is.na(p)) %>%
-      dplyr::arrange(t, i, j)
+      dplyr::arrange(
+        t, 
+        i, 
+        j
+        )
     # compute indirect utility
-    u <- compute_indirect_utility(df, beta, sigma, 
-                                  mu, omega)
+    u <- 
+      compute_indirect_utility(
+        df, 
+        beta, 
+        sigma, 
+        mu, 
+        omega
+        )
     # add u and e
-    df_choice <- data.frame(df, u, e) %>%
+    df_choice <- 
+      data.frame(
+        df, 
+        u, 
+        e
+        ) %>%
       tibble::as_tibble()
     # make choice
-    df_choice <- df_choice %>%
-      dplyr::group_by(t, i) %>%
+    df_choice <- 
+      df_choice %>%
+      dplyr::group_by(
+        t, 
+        i
+        ) %>%
       dplyr::mutate(q = ifelse(u + e == max(u + e), 1, 0)) %>%
       dplyr::ungroup()
     # return
@@ -48,37 +94,74 @@ compute_choice <-
   }
 # compute share
 compute_share <-
-  function(X, M, V, e, beta, sigma, 
-           mu, omega) {
+  function(
+    X, 
+    M, 
+    V, 
+    e, 
+    beta, 
+    sigma, 
+    mu, 
+    omega
+    ) {
     # constants
     T <- max(M$t)
     N <- max(V$i)
     J <- max(X$j)
     # compute choice
     df_choice <- 
-      compute_choice(X, M, V, e, beta, sigma, 
-                     mu, omega)
+      compute_choice(
+        X, 
+        M, 
+        V, 
+        e, 
+        beta, 
+        sigma, 
+        mu, 
+        omega
+        )
     # make share data
-    df_share <- df_choice %>%
-      dplyr::select(-dplyr::starts_with("v_"), -u, -e, -i) %>%
-      dplyr::group_by(t, j) %>%
+    df_share <- 
+      df_choice %>%
+      dplyr::select(
+        -dplyr::starts_with("v_"), 
+        -u, 
+        -e, 
+        -i
+        ) %>%
+      dplyr::group_by(
+        t, 
+        j
+        ) %>%
       dplyr::mutate(q = sum(q)) %>%
       dplyr::ungroup() %>%
-      dplyr::distinct(t, j, .keep_all = TRUE) %>%
+      dplyr::distinct(
+        t, 
+        j, 
+        .keep_all = TRUE
+        ) %>%
       dplyr::group_by(t) %>%
       dplyr::mutate(s = q/sum(q)) %>%
       dplyr::ungroup()
     # log share difference
-    df_share <- df_share %>%
+    df_share <- 
+      df_share %>%
       dplyr::group_by(t) %>%
       dplyr::mutate(y = log(s/sum(s * (j == 0)))) %>%
       dplyr::ungroup()
     return(df_share)
   }
 
-# NLLS objective function
-NLLS_objective_A3 <-
-  function(theta, df_share, X, M, V_mcmc, e_mcmc) {
+# nlls objective function
+nlls_objective_a3 <-
+  function(
+    theta, 
+    df_share, 
+    X, 
+    M, 
+    V_mcmc, 
+    e_mcmc
+    ) {
     # constants
     K <- length(grep("x_", colnames(X)))
     # extract parameters
@@ -88,8 +171,16 @@ NLLS_objective_A3 <-
     omega <- theta[2 * K + 2]
     # compute predicted share
     df_share_mcmc <-
-      compute_share(X, M, V_mcmc, e_mcmc, beta, sigma, 
-                    mu, omega)
+      compute_share(
+        X, 
+        M, 
+        V_mcmc, 
+        e_mcmc, 
+        beta, 
+        sigma, 
+        mu, 
+        omega
+        )
     # compute distance
     distance <- mean((df_share_mcmc$s - df_share$s)^2)
     # return
