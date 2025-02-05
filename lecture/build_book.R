27daf873839786691d0a718251069510c3610023
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2025-02-05 11:43:26 +0800

    updated assignment 2

--- lecture/build_book.R
+++ lecture/build_book.R
@@ -1,24 +1,29 @@
-# restart the session
-.rs.restartR()
+# Clear environment and restart R
+rm(list = ls())  # clear environment
+if(exists(".rs.restartR")) {
+  .rs.restartR()
+} else {
+  # Alternative approach if not in RStudio
+  try(dev.off(), silent = TRUE)  # clear plots
+  gc()  # garbage collection
+}
+
 file.create("docs/.nojekyll")
 
-# set the working directory
-setwd("lecture")
-
-# serve book
-bookdown::serve_book(dir = ".")
+# serve book - fixed directory path
+bookdown::serve_book(dir = "lecture")
 
 # render html book
 bookdown::render_book(
-  input = "index.Rmd", 
+  input = "lecture/index.Rmd", 
   output = "bookdown::gitbook",
   clean = TRUE
-  )
+)
 
 # render pdf book
 bookdown::render_book(
-  input = "index.Rmd", 
+  input = "lecture/index.Rmd", 
   output = "bookdown::pdf_book",
   clean = TRUE
-  )
+)
 
