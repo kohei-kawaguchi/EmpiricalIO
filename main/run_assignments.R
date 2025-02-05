8dbb0353c8b364128485c2176b5bc922780fd1f6
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2025-02-05 22:44:14 +0800

    added file to render markdown files

---
+++ main/run_assignments.R
@@ -0,0 +1,20 @@
+# Script to run all Rmd files in the assignment directory
+
+# Get list of all Rmd files in assignment directory
+rmd_files <- 
+  list.files(
+    path = "assignment",  
+    pattern = "\\.Rmd$",
+    full.names = TRUE
+  )
+
+# Render each file
+for (
+  file in rmd_files
+  ) {
+  render_file(
+    file_path = file
+  )
+}
+
+message("Finished rendering all Rmd files")
